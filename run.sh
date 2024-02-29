6757c105433db6ac0cd8cdf4d91506357956a22b
Joel Johansson <joel.k.johansson@gmail.com>
2024-02-29 11:03:19 +0100

    fix monitor ctlr c

--- run.sh
+++ run.sh
@@ -13,6 +13,7 @@ qemu-system-x86_64 \
   -hda buildroot/output/images/rootfs.ext4 \
   -nographic -append "root=/dev/sda rw console=ttyS0 acpi=off nokaslr" \
   -enable-kvm \
-  -netdev tap,id=t0,ifname=qemu_tap0,script=no,downscript=no -device e1000,netdev=t0 \
-  -netdev tap,id=t1,ifname=qemu_tap1,script=no,downscript=no -device e1000,netdev=t1 \
+  -netdev tap,id=t0,ifname=qemu_tap0,script=no,downscript=no -device igb,netdev=t0 \
+  -netdev tap,id=t1,ifname=qemu_tap1,script=no,downscript=no -device igb,netdev=t1 \
   -monitor unix:qemu-monitor-socket,server,nowait \
+  -serial mon:stdio \
